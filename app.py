72b9ae45f3a0c915b5526178de06b8c85939e106
Manan Jain <101595148+DataManan@users.noreply.github.com>
2023-08-31 19:23:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def find_largest(num1, num2, num3):
 
 def main():
     st.title("Find the Largest Number")
-    
+    st.subheader("I am Manan Jain, 22f1001259, and this is my week 8 graded assignment")
     num1 = st.number_input("Enter the first number:")
     num2 = st.number_input("Enter the second number:")
     num3 = st.number_input("Enter the third number:")
